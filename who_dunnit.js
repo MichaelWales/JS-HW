f69a0431c3889b2ba4f37fc236f3c129f72b4534
Michael Wales <michaelwales@gmail.com>
2020-04-13 20:07:58 +0100

    Adds Ep6

--- who_dunnit.js
+++ who_dunnit.js
@@ -1,7 +1,7 @@
 // Episode
 // Guess -
 // Reasoning -
-// Correction - 
+// Correction -
 
 // // Episode 1
 // // Guess - Miss Scarlet
@@ -58,16 +58,69 @@
 // const secondVerdict = `The murderer is ${murderer}.`;
 // console.log('Second Verdict: ', secondVerdict);
 
+// // Episode 4
+// // Guess - Miss Scarlet, Prof. Plum & Mrs. Peacock
+// // Reasoning - Suspect 1 & 2 aren't being changed. Suspect 3 is being
+// // reassigned to Colonel Mustard
+// // Correction - I don't think I really understand this homework. I thought
+// // there was only meant to be one output for each? V. Frustrating.
+// let suspectOne = 'Miss Scarlet';
+// let suspectTwo = 'Professor Plum';
+// let suspectThree = 'Mrs. Peacock';
+//
+// const declareAllSuspects = function() {
+//   let suspectThree = 'Colonel Mustard';
+//   return `The suspects are ${suspectOne}, ${suspectTwo}, ${suspectThree}.`;
+// }
+//
+// const suspects = declareAllSuspects();
+// console.log(suspects);
+// console.log(`Suspect three is ${suspectThree}.`);
 
-let suspectOne = 'Miss Scarlet';
-let suspectTwo = 'Professor Plum';
-let suspectThree = 'Mrs. Peacock';
 
-const declareAllSuspects = function() {
-  let suspectThree = 'Colonel Mustard';
-  return `The suspects are ${suspectOne}, ${suspectTwo}, ${suspectThree}.`;
+// // Episode 5
+// // Guess - Miss Scarlet in the Kitchen with the Revolver
+// // Reasoning - The murderer and place isn't changing. The weapon is changed.
+// // Correction -
+// const scenario = {
+//   murderer: 'Miss Scarlet',
+//   room: 'Kitchen',
+//   weapon: 'Candle Stick'
+// };
+//
+// const changeWeapon = function(newWeapon) {
+//   scenario.weapon = newWeapon;
+// }
+//
+// const declareWeapon = function() {
+//   return `The weapon is the ${scenario.weapon}.`;
+// }
+//
+// changeWeapon('Revolver');
+// const verdict = declareWeapon();
+// console.log(verdict);
+
+
+// Episode 6
+// Guess -
+// Reasoning -
+// Correction - 
+let murderer = 'Colonel Mustard';
+
+const changeMurderer = function() {
+  murderer = 'Mr. Green';
+
+  const plotTwist = function() {
+    murderer = 'Mrs. White';
+  }
+
+  plotTwist();
 }
 
-const suspects = declareAllSuspects();
-console.log(suspects);
-console.log(`Suspect three is ${suspectThree}.`);
+const declareMurderer = function () {
+  return `The murderer is ${murderer}.`;
+}
+
+changeMurderer();
+const verdict = declareMurderer();
+console.log(verdict);
