4d6e6a8ac870f264cdb81d9895f1945ddae60e81
Michael Wales <michaelwales@gmail.com>
2020-04-13 19:50:49 +0100

    Adds three episodes

--- who_dunnit.js
+++ who_dunnit.js
@@ -1,13 +1,59 @@
-// Episode 1
-const scenario = {
-  murderer: 'Miss Scarlet',
-  room: 'Library',
-  weapon: 'Rope'
-};
+// Episode
+// Guess -
+// Reasoning -
+// Correction -
 
-const declareMurderer = function() {
-  return `The murderer is ${scenario.murderer}.`;
-}
+// // Episode 1
+// // Guess - Miss Scarlet
+// // Reasoning - It's pretty obvious here. The murderer is set initially and not
+// // changed.
+// const scenario = {
+//   murderer: 'Miss Scarlet',
+//   room: 'Library',
+//   weapon: 'Rope'
+// };
+//
+// const declareMurderer = function() {
+//   return `The murderer is ${scenario.murderer}.`;
+// }
+//
+// const verdict = declareMurderer();
+// console.log(verdict);
 
-const verdict = declareMurderer();
-console.log(verdict);
+
+// // Episode 2
+// // Guess - Mrs. Peacock
+// // Reasoning - The murderer is set as Plum, then changed halfway through.
+// // Correction - Didn't remember that const can't be changed!
+// const murderer = 'Professor Plum';
+//
+// const changeMurderer = function() {
+//   murderer = 'Mrs. Peacock';
+// }
+//
+// const declareMurderer = function() {
+//   return `The murderer is ${murderer}.`;
+// }
+//
+// changeMurderer();
+// const verdict = declareMurderer();
+// console.log(verdict);
+
+
+// // Episode 3
+// // Guess - Mrs. Peacock
+// // Reasoning - The murderer is stored in a let and changed in the declareMurderer
+// // const.
+// // Correction - Not sure I understand this one.
+// let murderer = 'Professor Plum';
+//
+// const declareMurderer = function() {
+//   let murderer = 'Mrs. Peacock';
+//   return `The murderer is ${murderer}.`;
+// }
+//
+// const firstVerdict = declareMurderer();
+// console.log('First Verdict: ', firstVerdict);
+//
+// const secondVerdict = `The murderer is ${murderer}.`;
+// console.log('Second Verdict: ', secondVerdict);
